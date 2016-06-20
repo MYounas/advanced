c775bce1e99dc76e8b35d6c25e837dae5ed6368b
M-Younas <younus.ubit@outlook.com>
2016-06-20 10:57:25 +0500

    11 vid

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -12,6 +12,9 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+    public $first_name;
+    public $last_name;
+
 
 
     /**
@@ -22,6 +25,8 @@ class SignupForm extends Model
         return [
             ['username', 'filter', 'filter' => 'trim'],
             ['username', 'required'],
+            ['first_name', 'required'],
+            ['last_name', 'required'],
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
@@ -48,6 +53,8 @@ class SignupForm extends Model
         }
         
         $user = new User();
+        $user->first_name = $this->first_name;
+        $user->last_name = $this->last_name;
         $user->username = $this->username;
         $user->email = $this->email;
         $user->setPassword($this->password);
